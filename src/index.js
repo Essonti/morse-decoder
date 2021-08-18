f53f52458e943a526cd59cd448eeb5dec85814b9
Essonti <Strofim4enko@yandex.ru>
2021-08-18 23:21:22 +0300

    feat: add code and pass all tests

--- src/index.js
+++ src/index.js
@@ -37,9 +37,33 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+function decode(expr) {    
+    let sent = '';
+    let charNumber = expr.length / 10;
+    let charMorse = '';
+    let charBin = '';
+    let result = '';  
+    for (let i = 0; i < charNumber; i++) {
+        charBin = expr.slice(0, 10);
+        for (let j = 0; j < 5; j++) {            
+            if (j === 0) {result = (charBin.slice(-2, charBin.length))}
+            else {result = (charBin.slice(-2 - 2 * j, -2 * j))};
+            if (result == '10') {
+                charMorse = '.' + charMorse;
+            } else if (result == '11') {
+                charMorse = '-' + charMorse;
+            } else if (result == '**') {charMorse = ' '};            
+        };
+        for (let key in MORSE_TABLE) {
+            if (key === charMorse) {sent += MORSE_TABLE[key]};
+        };
+        if (charMorse == ' ') {sent += ' '};
+        charMorse = '';
+        expr = expr.slice(10);
+    };     
+    return sent;       
+};
+
 
 module.exports = {
     decode
